30f34f2bb1b9e09f74f9cc7141b50ea6ae0f522e
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 19:03:20 +0530

    Modify dataframe selection and add stop command
    Updated the data selection to include 'search_on' column and added a stop command after displaying the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,9 @@ st.write("The name on your Smoothie will be :",name_on_order)
 #session =get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe=session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe,use_container_width=True)
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 
 ingredients_list=st.multiselect(
     'choose upto 5 ingredients:',
@@ -36,6 +37,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
         st.subheader(fruit_chosen + " Nutrition Information ")
+        search=
         smoothiefroot_response=requests.get(f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
