88a46efefeca6f7e447a1d629f37182c66dd6ef6
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 19:03:52 +0530

    Remove unused variable 'search' from streamlit_app.py
    Removed unused variable 'search' from the code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
         st.subheader(fruit_chosen + " Nutrition Information ")
-        search=
+        #search=
         smoothiefroot_response=requests.get(f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
