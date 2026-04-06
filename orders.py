bfdd9dac6a17b6ddd66e6b99f829aa03e56f9f45
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 12:02:40 +0530

    Create orders.py
    added pending orders file

---
+++ orders.py
@@ -0,0 +1,39 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col,when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+  """ Orders that needs to be filled. """
+)
+
+#session =get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    
+    submitted=st.button('Submit')
+    
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try :
+            og_dataset.merge(edited_dataset
+                                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                                )
+            st.success("order(s) Updated !",icon="👍")
+        except:
+            st.write("Something Went wrong")
+        
+else:
+    st.success("There are no pending orders right now", icon="👍")
+
+
+
