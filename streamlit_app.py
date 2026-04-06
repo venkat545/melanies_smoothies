07009e82a76fc2d8bb7196d33d48e953c9dfaceb
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 17:28:26 +0530

    Update streamlit_app.py
    Added the api call to Smoothiefroot website using request library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
@@ -46,6 +47,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+smoothiefroot_response=requests.get("https://mu.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
         
