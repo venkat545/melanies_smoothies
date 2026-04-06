f7b8bb91974f7f963cab2b33df8252dd003d0e54
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 18:25:28 +0530

    Fix syntax error in subheader for fruit nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        st.subheader(fruit_chosen + " Nutrition Information "
+        st.subheader(fruit_chosen + " Nutrition Information ")
         smoothiefroot_response=requests.get(f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
