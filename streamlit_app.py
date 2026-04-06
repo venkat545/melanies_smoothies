494770a44b71b7e2a3f00fefdfe4bd5cb42cc927
Venkateshwar <69678941+venkat545@users.noreply.github.com>
2026-04-06 18:18:36 +0530

    Clean up smoothie order process
    Removed redundant API call and dataframe display for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ if ingredients_list:
     ingredients_string=''
     for fruits_chosen in ingredients_list:
         ingredients_string+=fruits_chosen+' '
+        smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -48,9 +51,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
 
         
